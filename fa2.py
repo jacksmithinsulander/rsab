8bfc01d61f14eba47ca0348340f77203c281c3ae
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-03-05 16:23:22 +0100

    fixed the fetches of all the relevant data, now we're making a 3 step FA, however I will optimise the code now, because its ugly

--- fa2.py
+++ fa2.py
@@ -1,19 +1,17 @@
-import time
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.remote.webdriver import By
 import selenium.webdriver.support.expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 import undetected_chromedriver as uc
-#import requests
 import cloudscraper
 
 driver = uc.Chrome()
 
-
-#driver.get("https://tokensniffer.com/token/eth/0x2b591e99afe9f32eaa6214f7b7629768c40eeb39")
 token_address = "0x2b591e99afe9f32eaa6214f7b7629768c40eeb39"
 token_chain_short = "eth"
 token_chain = "ethereum"
+token_chain_extra = "ether"
+token_lp_pair = "0x69d91b94f0aaf8e8a2586909fa77a5c2c89818d5"
 
 def tokensniffer_fa(token, chain):
 	driver.get("https://tokensniffer.com/token/" + chain + "/" + token)
@@ -25,11 +23,18 @@ def tokensniffer_fa(token, chain):
 tokensniffer_fa(token_address, token_chain_short)
 
 def dexscreener_fa(token, chain):
-	#driver.get("https://cfw.dexscreener.com/sc/dex:" + chain + ":" + token)
 	token_url = "https://cfw.dexscreener.com/sc/dex:" + chain + ":" + token + "/counter"
 	scraper = cloudscraper.create_scraper(delay=10,   browser={'custom': 'ScraperBot/1.0',})
 	r = scraper.get(token_url)
-	#data = r.json
 	print(r.text)
 
 dexscreener_fa(token_address, token_chain)
+
+def dextools_fa(lp, chain):
+	driver.get("https://www.dextools.io/app/en/" + chain + "/pair-explorer/" + lp)
+	dextools_score = WebDriverWait(driver, timeout=40).until(
+		EC.presence_of_element_located((By.XPATH, '//*[@id="progressDext"]/div/div/strong'))
+	)
+	print(dextools_score.text)
+
+dextools_fa(token_lp_pair, token_chain_extra)
