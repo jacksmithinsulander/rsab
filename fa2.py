59def48b94364963207847c360ae63294e7a2062
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-03-05 15:24:16 +0100

    made a lil cleanup

--- fa2.py
+++ fa2.py
@@ -6,11 +6,17 @@ from selenium.webdriver.support.wait import WebDriverWait
 import undetected_chromedriver as uc
 
 driver = uc.Chrome()
-driver.get("https://tokensniffer.com/token/eth/0x2b591e99afe9f32eaa6214f7b7629768c40eeb39")
-#sleep(2)
-score = WebDriverWait(driver, timeout=40).until(
-	EC.presence_of_element_located((By.XPATH, '//*[@id="__next"]/div/main/div[2]/div[2]/div[1]/table[1]/tbody/tr[1]/td/h2/span'))
-)
-#inal_score = [score.text for score in scores]
-#score = driver.find_element(By.XPATH, )
-print(score.text)
+
+
+#driver.get("https://tokensniffer.com/token/eth/0x2b591e99afe9f32eaa6214f7b7629768c40eeb39")
+token_address = "0x2b591e99afe9f32eaa6214f7b7629768c40eeb39"
+token_chain = "eth"
+
+def tokensniffer_fa(token, chain):
+	driver.get("https://tokensniffer.com/token/" + chain + "/" + token)
+	score = WebDriverWait(driver, timeout=40).until(
+		EC.presence_of_element_located((By.XPATH, '//*[@id="__next"]/div/main/div[2]/div[2]/div[1]/table[1]/tbody/tr[1]/td/h2/span'))
+	)
+	print(score.text)
+
+tokensniffer_fa(token_address, token_chain)
