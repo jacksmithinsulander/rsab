1dc7e5523fd46c18b57d283640bd6296a06c0cef
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-03-06 02:00:30 +0100

    Added checks for more contracts
    #.gitignore
    Added cache files
    
    #abi.py & swapList.py
    Added new contracts
    
    #poolScanner.py
    Updated code to handle multiple contracts
    Improved log-readability
    #Removed unused files
    datascraped.json and uniswap3abi.json

---
+++ swapList.py
@@ -0,0 +1,21 @@
+swapList = {
+    'uniswap3': {
+        'address': '0x1F98431c8aD98523631AE4a59f267346ea31F984',
+        'eventName': 'PoolCreated',
+        'poolAddress': 'pool'},
+    'sushiv2': {
+        'address': '0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac',
+        'eventName': 'PairCreated',
+        'poolAddress': 'pair'},
+    'apeswap': {
+        'address': '0xBAe5dc9B19004883d0377419FeF3c2C8832d7d7B',
+        'eventName': 'PairCreated',
+        'poolAddress': 'pair'},
+    'kyberswap': {
+        'address': '0x833e4083B7ae46CeA85695c4f7ed25CDAd8886dE',
+        'eventName': 'PoolCreated',
+        'poolAddress': 'pool'}
+}
+
+# Balancer still needs to be added, but has different syntax for their contracts.
+# DODO as well, unsure which contract to add.
