5ed7b506444e6a77649f6cb012cfb367fd24a7a9
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-03-08 13:33:38 +0100

    Start of Polygon scanner
    # poolScanner
    Rewrote slightly to fit new rpcList format
    
    # abi.py
    Added quickswap v3
    
    # rpcList.py
    New list for networks and their rpc links
    
    # swapList.py
    Rewrote to handle multiple networks
    
    # gitignore
    Added new apiKeys.py

--- poolScanner.py
+++ poolScanner.py
@@ -2,36 +2,33 @@ from datetime import datetime
 from time import sleep
 from web3 import Web3
 import db.main as db
-from apiKey import apiKey
 from swapList import swapList
+from rpcList import rpcList
 
 import abi
 
 
-netList = ['mainnet']
-
 while True:
     now = datetime.now().strftime("%H:%M:%S")
     print(f"# {now} ######################################")
-    for net in netList:
-        w3 = Web3(Web3.HTTPProvider(
-            f"https://{net}.infura.io/v3/{apiKey}"))
+    for net in rpcList:
+        w3 = Web3(Web3.HTTPProvider(rpcList[net]['link']))
         lastBlock = w3.eth.block_number
-        fromBlock = lastBlock - 50
+        fromBlock = lastBlock - 5000
         print(f"  # Checking {net} # Blocks {fromBlock}-{lastBlock}")
         #print(f"Connection to {net}: {w3.isConnected()}")
-        for swap in swapList:
+        for swap in swapList[net]:
             print(
                 f"    # Checking for pools by {swap}")
             contract = w3.eth.contract(address=Web3.toChecksumAddress(
-                swapList[swap]['address']), abi=abi.swaps[swap])
+                swapList[net][swap]['address']), abi=abi.swaps[swap])
 
             exec(
-                f"getPool = contract.events.{swapList[swap]['eventName']}.getLogs")
+                f"getPool = contract.events.{swapList[net][swap]['eventName']}.getLogs")
             latestPools = getPool(fromBlock=fromBlock)
 
             for pool in latestPools:
-                poolAddress = pool['args'][swapList[swap]
+                poolAddress = pool['args'][swapList[net][swap]
                                            ['poolAddress']]
                 if (db.checkIfSaved(poolAddress)):
                     print(f'      Already saved {poolAddress}')
---
+++ rpcList.py
@@ -0,0 +1,10 @@
+import apiKeys
+
+rpcList = {
+    'ethereum': {
+        'link': f"https://mainnet.infura.io/v3/{apiKeys.infura}"
+    },
+    'polygon': {
+        'link': f"https://polygon-bor.publicnode.com"
+    }
+}
