847b15266ac10d959652f5ee0b126c8abeded344
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-03-17 20:55:06 +0100

    Started doing the massive cleanup and rewrite in order to actually make the setup script set up the config file

--- setup.py
+++ setup.py
@@ -27,98 +27,12 @@ Planing flow
 				2.2.2.6.2. Finetune pass thresholds
 3. Pass selection into a new config file
 """
-print("Hello you filthy, degenerate ape. Lets set up this shitcoinbot of yours")
-print("=======================================================================")
-print('First of what EVMs would you like to "trade"(read gamble) on? Answer with the coresponding capitalized letters, separated by commas')
-chains = input("Ethereum mainnet (E), Arbitrum (A), Optimism (O), Polygon (P), Avalanche (V), Fantom (F), Binance smart chain (B)\n")
-#print(chains)
-#match chains: 
-#	case "E":
-#		print("You choose Ethereum main net")
-#	case "A":
-#		print("You choose Arbitrum")
-#	case "O":
-#		print("You choose Optimism")
-#	case "P":
-#		print("You choose Polygon")
-#	case "V":
-#		print("You choose Avalanche")
-#	case "F":
-#		print("You choose Fantom")
-#	case "B":
-#		print("You choose binance smart chain")
-#	case other:
-#		print("Input error, you messed up, try again")
-#apparently the debian package didnt have python3.10, so righ tnow I have to use
-#this outdated and totally AWFUL syntax
-#the idea however is that i will iterate over this statement, and write it as 
-#a function that builds up an object, and once that object is created
-#the program will push it into a new file, which will act as the configuration 
-#file, then everything else is taking in parameters from that program
+import os.path
+import json
 
-chains_arr = chains.split(", ")
-print(chains_arr)
+#chain_chooser(chains_arr)
+#param_choose_results(param_choose)
 
-#now that i see this its kind of stupid, figure out a way to write it cleaner
-def chain_chooser(chainarr):
-	for x in chainarr:
-		if x == "E":
-			print("You have chosen Ethereum mainnet.")
-		elif x == "A":
-			print("You have chosen Arbitrum.")
-		elif x == "O":
-			print("You have chosen Optimism.")
-		elif x == "P":
-			print("You have chosen Polygon.")
-		elif x == "V":
-			print("You have chosen Avalanche.")
-		elif x == "F":
-			print("You have chosen Fantom.")
-		elif x == "B":
-			print("You have chosen Binance Smart Chain.")
-		else:
-			print("Input error. Please try again.\n")
-chain_chooser(chains_arr)
-
-print("=======================================================================")
-print("Ait, now for the trading bot parameters, such as stop loss and profit taking")
-param_choose = input("Do you want the standard (1) or custom (2) settings? Show standard settings (3)\n")
-
-def param_choose_results(choice):
-	if choice == "1":
-		print("You selected the standard settings. wagmi")
-	elif choice == "2":
-		print("You want to be adventurous and tinker with you own settings")
-	elif choice == "3":
-		print("=======================================================================")
-		print('The standard settings are inspired by the "little old lady" trading strategy.')
-		print("The idea is that it basically takes profit each time that the value you hold is double to the value you initially bought")
-		print('Just google (or use a better search engine like searx / gigablast / yandex) for the "little old lady" strategy to get a better idea of how it works')
-		print('You are free to change booth the amount of profit taking and the actual levels of profit taking')
-		print('Other than that, standard is also that the bot will rebalance your Eth / stables so that you hold "dry powder" for booth kinds of pairs')
-		print('You can turn the rebalancing off')
-		print('The bot will buy for 1% of the combined worth of eth / stables that your wallet has for each entry')
-		print('You can change this to a static number if you like')
-		print('Also, standard is to have a tiling stop loss att the 200 MA (read on the 4h chart)')
-		print("This means that it will take this means that for the first (200 * 4h = 800 h = 33.33 days) you're without a stoploss")
-		print('You can either choose to change this to a static stoploss, or a timebased one or a combination of stoplosses')
-		print('Our "sophisticated" FA, is just that we check the token on dextools ratings, dexscreener ratings and tokensniffer')
-		print('Our standard settings are that it has to reach a certain amount of "points" to "pass" each step')
-		print('And in order to be a valid entry, a token must fullfill at least 2/3 passes')
-		print('You can booth choose to finetune the points threshold, as well as the pass threshold')
-		print('Or (NOT RECOMENDED) decide to remove the FA completly')
-		print('However that would be stupid, because the fa is mostly checking the token on a contract level')
-		print('Checking things like if it is possible to sell the token, if the liquidity is locked etc.')
-	else:
-		print("Didnt understand your input, try again\n")
-
-param_choose_results(param_choose)
-
-print("=======================================================================")
-change_selection = input("Do you want to change all of these parameters (A) or only some (B) of them?\n")
-
-#thinking through it all now and realising that this will need a massive rewrite
-#at a later time, with a LOT of return functions
 def profit_taking_config():
 	print("=======================================================================")
 	profit_taking = input('How many % profits do you want to take on each target levels? Answer with numbers ended by % sign, i.e. "50 %" or "100 %"(if you want to sell it all on first target)\n')
@@ -205,4 +119,87 @@ def param_selection_define(selecs):
 	else:
 		print("Input error, try not missing the a or b keys next time.")
 
-param_selection_define(change_selection)
+def custom_choose():
+	print("=======================================================================")
+	print('The standard settings are inspired by the "little old lady" trading strategy.')
+	print("The idea is that it basically takes profit each time that the value you hold is double to the value you initially bought")
+	print('Just google (or use a better search engine like searx / gigablast / yandex) for the "little old lady" strategy to get a better idea of how it works')
+	print('You are free to change booth the amount of profit taking and the actual levels of profit taking')
+	print('Other than that, standard is also that the bot will rebalance your Eth / stables so that you hold "dry powder" for booth kinds of pairs')
+	print('You can turn the rebalancing off')
+	print('The bot will buy for 1% of the combined worth of eth / stables that your wallet has for each entry')
+	print('You can change this to a static number if you like')
+	print('Also, standard is to have a tiling stop loss att the 200 MA (read on the 4h chart)')
+	print("This means that it will take this means that for the first (200 * 4h = 800 h = 33.33 days) you're without a stoploss")
+	print('You can either choose to change this to a static stoploss, or a timebased one or a combination of stoplosses')
+	print('Our "sophisticated" FA, is just that we check the token on dextools ratings, dexscreener ratings and tokensniffer')
+	print('Our standard settings are that it has to reach a certain amount of "points" to "pass" each step')
+	print('And in order to be a valid entry, a token must fullfill at least 2/3 passes')
+	print('You can booth choose to finetune the points threshold, as well as the pass threshold')
+	print('Or (NOT RECOMENDED) decide to remove the FA completly')
+	print('However that would be stupid, because the fa is mostly checking the token on a contract level')
+	print('Checking things like if it is possible to sell the token, if the liquidity is locked etc.')
+	print("=======================================================================")
+	change_selection = input("Do you want to change all of these parameters (A) or only some (B) of them?\n")
+	#if change_selection ==
+
+def param_choose_results():
+	print("=======================================================================")
+	print("Ait, now for the trading bot parameters, such as stop loss and profit taking")
+	param_choose = input("Do you want the standard (1) or custom (2) settings?\n")
+	if param_choose == "1":
+		print("You selected the standard settings. wagmi")
+		return 0
+	elif param_choose == "2":
+		print("You want to be adventurous and tinker with you own settings")
+		return 1
+	else:
+		print("Didnt understand your input, try again\n")
+
+def chain_chooser():
+	print("Hello you filthy, degenerate ape. Lets set up this shitcoinbot of yours")
+	print("=======================================================================")
+	print('First of what EVMs would you like to "trade"(read gamble) on? Answer with the coresponding capitalized letters, separated by commas')
+	chains = input("Ethereum mainnet (E), Arbitrum (A), Optimism (O), Polygon (P), Avalanche (V), Fantom (F), Binance smart chain (B) or ALL (ALL)\n")
+	chainarr = chains.split(", ")
+	for x in chainarr:
+		if x == "E":
+			print("You have chosen Ethereum mainnet.")
+		elif x == "A":
+			print("You have chosen Arbitrum.")
+		elif x == "O":
+			print("You have chosen Optimism.")
+		elif x == "P":
+			print("You have chosen Polygon.")
+		elif x == "V":
+			print("You have chosen Avalanche.")
+		elif x == "F":
+			print("You have chosen Fantom.")
+		elif x == "B":
+			print("You have chosen Binance Smart Chain.")
+		elif x == "ALL":
+			print("You choose all")
+		else:
+			print("Input error. Please try again.\n")
+	return chainarr
+
+def conf_setup():
+	chains = chain_chooser()
+	print(chains)
+	params = param_choose_results()
+	print(bool(params))
+
+check_file = os.path.isfile("./conf.json")
+
+def conf_checker(file_exists):
+	if file_exists:
+		print("Configuration file already exists")
+	else:
+		print("File does not exist")
+		print("Let's set it up!")
+		conf_setup()
+
+conf_checker(check_file)
+
+
+#param_selection_define(change_selection)
