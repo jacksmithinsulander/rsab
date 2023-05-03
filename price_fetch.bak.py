47936f62c7553ebd213d4431b3588c9414fd4e10
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-05-03 16:23:54 +0200

    fix

---
+++ price_fetch.bak.py
@@ -0,0 +1,56 @@
+from moralis import evm_api
+import time
+from moralis_key import moralis_key
+
+api_key = moralis_key
+
+price_data = []
+
+unix_timestamp = str(time.time())
+
+params_date = {
+	"date": unix_timestamp, 
+	"chain": "eth", 
+}
+
+current_block = evm_api.block.get_date_to_block(
+	api_key=api_key,
+	params=params_date,
+)
+#print(current_block['timestamp'])
+
+#Mockdata with a random shitcoin that is new rn
+for to_block in range(16776982, int(current_block['timestamp']), 10):
+	params = {
+		"address": "0x8e893280FABc738f539e479FD469E5eeBb710E91",
+		"chain": "eth",
+		"to_block": to_block
+	}
+	result = evm_api.token.get_token_price(
+		api_key=api_key,
+		params=params,
+	)
+	price_data.append(result)
+
+print(price_data)
+
+price_arr = [obj["usdPrice"] for obj in price_data]
+
+#fint bottom and top (only bottom PRIOR to top)
+def btmtop_finder(prices):
+	n = len(prices)
+	highest = max(prices)
+	highest_index = prices.index(highest)
+	lowest = min(prices[:highest_index])
+	print("The highest price is " + str(highest) + 
+		" And the lowest price before that is " + str(lowest))
+	fibretracement_finder(highest, lowest)
+
+#function that calculates .786 retracement and .618 retracement
+def fibretracement_finder(btm, top):
+	x = top - btm
+	_786 = top - (0.786 * x)
+	_618 = top - (0.618 * x)
+	print(_786, _618)
+
+btmtop_finder(price_arr)
--- price_fetch.py
+++ price_fetch.py
@@ -1,56 +1,23 @@
-from moralis import evm_api
-import time
-from moralis_key import moralis_key
+from web3 import Web3 
+import json
 
-api_key = moralis_key
+w3 = Web3(Web3.HTTPProvider("http://larslund.in:8545/"))
 
-price_data = []
+result = w3.isConnected()
+print('Web3 is connected? ', result)
 
-unix_timestamp = str(time.time())
+contract_abi = json.loads('[{"inputs":[{"internalType":"string","name":"name","type":"string"},{"internalType":"string","name":"symbol","type":"string"},{"internalType":"uint8","name":"decimals","type":"uint8"},{"internalType":"uint256","name":"supply","type":"uint256"},{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"feeWallet","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]')
 
-params_date = {
-	"date": unix_timestamp, 
-	"chain": "eth", 
-}
+contract_address = "0xE238Ecb42C424E877652AD82d8A939183A04C35f"
 
-current_block = evm_api.block.get_date_to_block(
-	api_key=api_key,
-	params=params_date,
+contract = w3.eth.contract(address=contract_address, abi=contract_abi)
+
+current_block = w3.eth.get_block('latest')['number']
+
+events = contract.events.transfer().getPastEvents(
+	"earliest", "latest",
+	{"fromBlock": 40683048, "toBlock": current_block }
 )
-#print(current_block['timestamp'])
 
-#Mockdata with a random shitcoin that is new rn
-for to_block in range(16776982, int(current_block['timestamp']), 10):
-	params = {
-		"address": "0x8e893280FABc738f539e479FD469E5eeBb710E91",
-		"chain": "eth",
-		"to_block": to_block
-	}
-	result = evm_api.token.get_token_price(
-		api_key=api_key,
-		params=params,
-	)
-	price_data.append(result)
-
-print(price_data)
-
-price_arr = [obj["usdPrice"] for obj in price_data]
-
-#fint bottom and top (only bottom PRIOR to top)
-def btmtop_finder(prices):
-	n = len(prices)
-	highest = max(prices)
-	highest_index = prices.index(highest)
-	lowest = min(prices[:highest_index])
-	print("The highest price is " + str(highest) + 
-		" And the lowest price before that is " + str(lowest))
-	fibretracement_finder(highest, lowest)
-
-#function that calculates .786 retracement and .618 retracement
-def fibretracement_finder(btm, top):
-	x = top - btm
-	_786 = top - (0.786 * x)
-	_618 = top - (0.618 * x)
-	print(_786, _618)
-
-btmtop_finder(price_arr)
+for event in events:
+	print(event)
