fbabbab8874b4e3299fe66794d9d09b9a1cf46a8
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-06-28 17:05:59 +0200

    fixed the rpc list, still need some errorhandling for it

--- modules/onchain/rpc_list.py
+++ modules/onchain/rpc_list.py
@@ -1,19 +1,67 @@
-#import api_keys
-
 rpc_list = {
     'ethereum': {
-        'links': [f"https://endpoints.omniatech.io/v1/eth/mainnet/public"],
+        'links': ["https://endpoints.omniatech.io/v1/eth/mainnet/public", 
+        	"https://eth.llamarpc.com", 
+        	"https://rpc.ankr.com/eth",
+        	"https://ethereum.publicnode.com",
+        	"https://1rpc.io/eth",
+        	"https://rpc.builder0x69.io",
+        	"https://rpc.mevblocker.io",
+        	"https://rpc.flashbots.net",
+        	"https://cloudflare-eth.com",
+        	"https://eth-rpc.gateway.pokt.network",
+        	"https://ethereum.blockpi.network/v1/rpc/public",
+        	"https://rpc.payload.de",
+        	"https://api.zmok.io/mainnet/oaen6dy8ff6hju9k",
+        	"https://eth.api.onfinality.io/public",
+        	"https://core.gashawk.io/rpc",
+        	"https://rpc.eth.gateway.fm",
+        	"https://eth.meowrpc.com",
+        	"https://eth.drpc.org",
+        	"https://mainnet.gateway.tenderly.co",
+        	"https://gateway.tenderly.co/public/mainnet"
+        	],
         'short': "eth",
         'extra': "ether"
     },  
     'polygon': {
-        'links': ["https://polygon-bor.publicnode.com", "https://polygon.blockpi.network/v1/rpc/public", "https://rpc.ankr.com/polygon"],
-        # "https://polygon.llamarpc.com",
+        'links': ["https://polygon-bor.publicnode.com", 
+        	"https://polygon.blockpi.network/v1/rpc/public", 
+        	"https://rpc.ankr.com/polygon",
+        	"https://rpc-mainnet.maticvigil.com",
+        	"https://polygon-rpc.com",
+        	"https://rpc-mainnet.matic.network",
+        	"https://rpc-mainnet.matic.quiknode.pro",
+        	"https://matic-mainnet.chainstacklabs.com",
+        	"https://matic-mainnet-full-rpc.bwarelabs.com",
+        	"https://matic-mainnet-archive-rpc.bwarelabs.com",
+        	"https://poly-rpc.gateway.pokt.network",
+        	"https://polygonapi.terminet.io/rpc",
+        	"https://1rpc.io/matic",
+        	"https://polygon-bor.publicnode.com",
+        	"https://polygon.blockpi.network/v1/rpc/public",
+        	"https://polygon.api.onfinality.io/public",
+        	"https://polygon.drpc.org",
+        	"https://polygon.gateway.tenderly.co",
+        	"https://gateway.tenderly.co/public/polygon"
+        	],
         'short': "polygon",
         'extra': "polygon"
     },
     'arbitrum': {
-        'links': ["https://arbitrum.blockpi.network/v1/rpc/public", "https://endpoints.omniatech.io/v1/arbitrum/one/public"],
+        'links': ["https://arbitrum.blockpi.network/v1/rpc/public", 
+        	"https://endpoints.omniatech.io/v1/arbitrum/one/public",
+        	"https://arb1.arbitrum.io/rpc",
+        	"https://rpc.ankr.com/arbitrum",
+        	"https://1rpc.io/arb",
+        	"https://arbitrum.getblock.io/api_key/mainnet",
+        	"https://arbitrum.blockpi.network/v1/rpc/public",
+        	"https://endpoints.omniatech.io/v1/arbitrum/one/public",
+        	"https://arbitrum.api.onfinality.io/public",
+        	"https://rpc.arb1.arbitrum.gateway.fm",
+        	"https://arbitrum-one.publicnode.com",
+        	"https://arbitrum.meowrpc.com"
+        	],
         'short': "arbitrum",
         'extra': "arbitrum"
     }
