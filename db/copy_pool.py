259e59fb313df71281b30af727076bc4acf8c3a8
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-10-10 19:04:28 +0200

    Update logger formatting

--- db/copy_pool.py
+++ db/copy_pool.py
@@ -1,31 +1,11 @@
 from loguru import logger
+
+
 def main(con, from_table, to_table, address):
     sql = f"""
-        INSERT INTO {to_table} ( net,
-            net_short,
-            net_extra,
-            pool_main_contract,
-            pool_address,
-            time_created,
-            token1_address,
-            token1_symbol,
-            token2_address,
-            token2_symbol,
-            dex,
-            creation_block)
-        SELECT net,
-            net_short,
-            net_extra,
-            pool_main_contract,
-            pool_address,
-            time_created,
-            token1_address,
-            token1_symbol,
-            token2_address,
-            token2_symbol,
-            dex,
-            creation_block FROM {from_table} 
-        WHERE pool_address="{address}";
+        INSERT INTO {to_table} ( net, net_short, net_extra, pool_main_contract, pool_address, time_created, token1_address, token1_symbol, token2_address, token2_symbol, dex, creation_block)
+        SELECT net, net_short, net_extra, pool_main_contract, pool_address, time_created, token1_address, token1_symbol, token2_address, token2_symbol, dex, creation_block
+        FROM {from_table} WHERE pool_address="{address}";
     """
     logger.debug(sql)
     with con:
