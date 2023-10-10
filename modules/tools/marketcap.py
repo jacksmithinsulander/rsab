dc37eb94bfe3f30bbe6fb087336c960058ae74ea
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-10-10 21:24:30 +0200

    more loguru added

--- modules/tools/marketcap.py
+++ modules/tools/marketcap.py
@@ -1,8 +1,10 @@
 from modules.tools.price import get_price
+from loguru import logger
+
 
 def calculate_marketcap(token1, token2, lp, abi):
     price, token = get_price(token1, token2, lp, abi)
     total_supply = token.functions.totalSupply().call()
     marketcap = price * total_supply
-    print(marketcap)
+    logger.debug(f"marketcap = {marketcap}")
     return marketcap
