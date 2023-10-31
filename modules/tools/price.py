54eef1629da9cac265842c6ebd0e96e0fe917fa8
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-10-31 01:50:00 +0100

    get_price update
    ### \modules\tools\price.py
    - Commented the test date
    - Rewrote get_price to only take a w3 (balancer) and the lp information
    
    ### \modules\ta\ta_iterate.py
    - Changed get_price() call to new format
    
    ### \modules\onchain\abi.py
    - Replaced erc20 with the abi used in modules\tools\price.py

--- modules/tools/price.py
+++ modules/tools/price.py
@@ -1,25 +1,37 @@
 from web3 import Web3
 from loguru import logger
+from modules.onchain import abi
+
+_net = 1
+_net_short = 2
+_net_extra = 3
+_pool_main_contract = 4
+_pool_address = 5
+_time_created = 6
+_token1_address = 7
+_token1_symbol = 8
+_token2_address = 9
+_token2_symbol = 10
+_dex = 11
+_creation_block = 12
 
 # TEST DATA
-token_address = "0xe4eFDd2eb216A4620cfA55c5cC67Bd09DC64Ff24"
-lp_address = "0x1D9992600D22D336E9aB9F0807989FEb945aEDCe"
-weth_address = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+# token_address = "0xe4eFDd2eb216A4620cfA55c5cC67Bd09DC64Ff24"
+# lp_address = "0x1D9992600D22D336E9aB9F0807989FEb945aEDCe"
+# weth_address = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
 
-erc20_abi = '[{"constant": true,"inputs": [],"name": "name","outputs": [{"name": "","type": "string"}],"payable": false,"stateMutability": "view","type": "function"},{"constant": false,"inputs": [{"name": "_spender","type": "address"},{"name": "_value","type": "uint256"}],"name": "approve","outputs": [{"name": "","type": "bool"}],"payable": false,"stateMutability": "nonpayable","type": "function"},{"constant": true,"inputs": [],"name": "totalSupply","outputs": [{"name": "","type": "uint256"}],"payable": false,"stateMutability": "view","type": "function"},{"constant": false,"inputs": [{"name": "_from","type": "address"},{"name": "_to","type": "address"},{"name": "_value","type": "uint256"}],"name": "transferFrom","outputs": [{"name": "","type": "bool"}],"payable": false,"stateMutability": "nonpayable","type": "function"},{"constant": true,"inputs": [],"name": "decimals","outputs": [{"name": "","type": "uint8"}],"payable": false,"stateMutability": "view","type": "function"},{"constant": true,"inputs": [{"name": "_owner","type": "address"}],"name": "balanceOf","outputs": [{"name": "balance","type": "uint256"}],"payable": false,"stateMutability": "view","type": "function"},{"constant": true,"inputs": [],"name": "symbol","outputs": [{"name": "","type": "string"}],"payable": false,"stateMutability": "view","type": "function"},{"constant": false,"inputs": [{"name": "_to","type": "address"},{"name": "_value","type": "uint256"}],"name": "transfer","outputs": [{"name": "","type": "bool"}],"payable": false,"stateMutability": "nonpayable","type": "function"},{"constant": true,"inputs": [{"name": "_owner","type": "address"},{"name": "_spender","type": "address"}],"name": "allowance","outputs": [{"name": "","type": "uint256"}],"payable": false,"stateMutability": "view","type": "function"},{"payable": true,"stateMutability": "payable","type": "fallback"},{"anonymous": false,"inputs": [{"indexed": true,"name": "owner","type": "address"},{"indexed": true,"name": "spender","type": "address"},{"indexed": false,"name": "value","type": "uint256"}],"name": "Approval","type": "event"},{"anonymous": false,"inputs": [{"indexed": true,"name": "from","type": "address"},{"indexed": true,"name": "to","type": "address"},{"indexed": false,"name": "value","type": "uint256"}],"name": "Transfer","type": "event"}]'
-
-eth = "https://eth.llamarpc.com"
-web3 = Web3(Web3.HTTPProvider(eth))
-logger.debug(f"Is web3 connected? {web3.is_connected()}")
+# eth = "https://eth.llamarpc.com"
+# web3 = Web3(Web3.HTTPProvider(eth))
+# logger.debug(f"Is web3 connected? {web3.is_connected()}")
 
 
-def get_price(token1, token2, lp, abi):
-    token = web3.eth.contract(address=token1, abi=abi)
-    reserve = web3.eth.contract(address=token2, abi=abi)
-    token_float = token.functions.balanceOf(lp).call()
+def get_price(w3, lp):
+    token = w3.eth.contract(address=lp[_token1_address], abi=abi.erc20)
+    reserve = w3.eth.contract(address=lp[_token2_address], abi=abi.erc20)
+    token_float = token.functions.balanceOf(lp[_pool_address]).call()
     token_decimals = token.functions.decimals().call()
     token_amount = token_float / 10 ** token_decimals
-    reserve_float = reserve.functions.balanceOf(lp).call()
+    reserve_float = reserve.functions.balanceOf(lp[_pool_address]).call()
     reserve_decimals = reserve.functions.decimals().call()
     reserve_amount = reserve_float / 10 ** reserve_decimals
     price = reserve_amount / token_amount
